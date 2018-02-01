cfc60158c50f3b86dccb86cbffe2b7b7de8c374d
LionPK <praneed.kla@northbkk.ac.th>
2018-02-01 16:04:29 +0700

    admin function

---
+++ application/libraries/mailer/mailer_config.php
@@ -0,0 +1,13 @@
+<?php
+
+		define('GUSER', 'youremail@gamil.com'); // email address
+        define('GPWD', 'yourpassword'); // email password
+        define('HOST', 'smtp.gmail.com'); // email server host
+        define('PORT', 465); // email server port
+
+        /**
+         * Gmail host smtp.gmail.com
+         * Gmail port 465
+         */
+
+?>
